6030e5c5bb3003f26bf5005581918f4eb7a58989
Andrew Rocchi <andrewroc30@gmail.com>
2019-04-04 01:40:37 -0400

    yee

--- QLearner.py
+++ QLearner.py
@@ -12,18 +12,18 @@ def getStockDataVec(key):
 
 actions = ['buy', 'sell', 'hold']
 #Hyparameters
-gamma  = 0.9
+gamma = 0.9
 alpha = 0.1 #learning rate
 epsilon = 0.1
-max_epsilon=1.0
-min_epsilon=0.01
+max_epsilon = 1.0
+min_epsilon = 0.01
 decay_rate = 0.01
 all_epochs = []
 all_penalities = []
 penalities = 0
 
 
-Q = None
+q_table = None
 balance = 1000
 
 
@@ -37,14 +37,15 @@ for i in range(1, 100001):
     while not done:
         if random.uniform(0, 1) < epsilon:
             #action = env.action_space.sample()
+            action = actions.sample()
         else:
-            #action = np.argmax(q_table[state])
+            action = np.argmax(q_table[state])
         #next_state, reward, done, info = env.step(action)
-        #next_max = np.max(q_table[next_state])
+        next_max = np.max(q_table[next_state])
         q_table[state, action] = q_table[state, action] + alpha * (reward + gamma * next_max - q_table[state, action])
         if reward == -10:
             penalities += 1
-        #state = next_state
+        state = next_state
         epochs += 1
     epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-0.1 * epsilon)
     if i % 100 == 0:
--- thing.py
+++ thing.py
@@ -5,7 +5,7 @@ def getLastNPrices(numDays, row, v):
     lastNPrices = np.zeros(numDays)
     if row >= numDays:
         for i in range(numDays):
-            lastNPrices[i] = v[row - numDays + i]
+            lastNPrices[i] = v[row - numDays + i + 1]
     else:
         avg = 0
         for val in v:
@@ -13,7 +13,11 @@ def getLastNPrices(numDays, row, v):
         avg = avg / len(v)
         for i in range(numDays):
             lastNPrices[i] = avg
+    lastNPrices[-1] = v[row]
     return lastNPrices
+
+
+#print(getLastNPrices(3, 5, [2, 3, 4, 5, 6, 1]))
 
 def pastPrices(v, numDays):
     newV = np.zeros((len(v), numDays))
@@ -28,7 +32,50 @@ closing_prices = data_2011.loc[:,"Close"]
 closing_prices = closing_prices.values
 
 
-#vec = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-#print(pastPrices(vec, 2))
-#print(pastPrices(vec, 1))
-print(pastPrices(closing_prices, 10))
+def updateWeights(difference, features, weights, alpha):
+    return  [x+y for x,y in zip(weights,[i*(alpha * difference) for i in features])]
+
+
+def qState(features, weights):
+    return np.dot(features, weights)
+
+
+#state: [numStocks, account, prices]
+def calcReward(curState, nextState):
+    return ((nextState[0] * nextState[len(nextState) - 1]) + nextState[1]) - ((curState[0] * curState[len(curState) - 1]) + curState[1])
+
+
+ def difference(reward, gamma, actions, curState, action, numDays, v):
+     x, index = curState
+     max = 2.2250738585072014e-308
+     account = x[1]
+     num_stocks = x[0]
+     next_state = None
+     for a in actions:
+         if x[0] != 0 and a == "s":
+             next_state = newState(v, curState, a, numDays)
+     return reward
+
+def newState(v, cur_state, action, numDays):
+    x,ind = cur_state
+    account = x[1]
+    num_stocks = x[0]
+    if(action == "s"):
+        account  += x[-1]
+        num_stocks -= 1
+    if(action == "b"):
+        account -= x[-1]
+        num_stocks += 1
+    return ([num_stocks,account] + (getLastNPrices(numDays,ind+1,v).tolist()) ,ind+1)
+
+#print(new_state([2,3,4,5,6,1],([0, 100, 3, 4, 5], 3),"b",3))
+#print(new_state([2,3,4,5,6,1],([0, 100, 3, 4, 5], 3),"h",3))
+#print(new_state([2,3,4,5,6,1],([0, 100, 3, 4, 5], 3),"s",3))
+
+
+
+
+
+#state1 = ([0, 1000, 1, 1, 1], 1)
+#state2 = ([1, 999, 1, 1, 5], 2)
+#print(calcReward(state1, state2))
